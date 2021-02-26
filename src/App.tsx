fc33ebf7a69559355ba89bfe45b99aa6bb1917bd
Gustavo Felix <gusfelix2015@gmail.com>
2021-02-25 21:50:21 -0300

    Day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Experiencebar } from "./components/ExperienceBar";
+
+import "./styles/global.css";
+
 function App() {
-  return <h1>Hello Nlw</h1>;
+  return (
+    <div className="container">
+      <Experiencebar />
+    </div>
+  );
 }
 
 export default App;
